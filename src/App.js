6ec12a2cdb5061a04d46e3151f43ea6b861f9778
Agustinmiquel <agumiquel@hotmail.com>
2022-09-09 23:19:21 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 
 import './App.css';
+import './estilo.css';
 import Contacts from './Components/Contacts/Contacts';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
+import AboutUs from './Components/AboutUs/AboutUs';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <AboutUs/>
       <Contacts/>
       <Footer/>
+
     </div>
   );
 }
---
+++ src/Components/AboutUs/AboutUs.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <div>
+      <div class="Nosotros">
+<h3>CONOCE MAS SOBRE NOSOTROS</h3>
+    <p> Bienvenidos a nuestra pagina web. Aqui encontraran nuestras promociones y descuentos en las mejores marcas de autos de alta gama. 
+    </p>
+</div>
+    </div>
+  )
+}
+
+export default AboutUs
